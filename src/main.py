866b821ff25931d380a7b5210f3ced2080e9d804
Andy Lai <Andy04Lai@gmail.com>
2024-07-30 14:44:12 +0930

    Test node created

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+    
+if __name__ == "__main__":
+    test = TextNode("This is a text node", "bold", "https://www.google.com")
+    print(test)
+
+
---
+++ src/textnode.py
@@ -0,0 +1,18 @@
+class TextNode():
+    def __init__(self, text, text_type, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+            return True
+        return False
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+if __name__ == "__main__":
+    test = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(test)
+
