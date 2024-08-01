c437c889f43399fa52f51a8bbef0b0ca2e6d252c
Andy Lai <Andy04Lai@gmail.com>
2024-08-01 17:44:05 +0930

    TextNode to Leafnode + Tests, Merged HTML Nodes into 1 file

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 class HTMLNode():
     def __init__(self, tag = None, value = None, children = None, props = None):
         self.tag = tag
@@ -18,4 +21,32 @@ class HTMLNode():
 
     def __repr__(self):
         return f"HTML NODE: tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props}"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return self.value
+        if self.props != None:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag=None, children=None, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if(self.tag == None):
+            raise ValueError("Parent Node has no tag")
+        
+        if(len(self.children) == 0):
+            raise ValueError("Parent Node contains no children")
+        
+        return f"<{self.tag}{self.props_to_html()}>" + reduce(lambda acc, x: acc + x.to_html(), self.children, "") + f"</{self.tag}>"
+
         
