ebc96db23b6ee901633b251cdff9e679807f4afc
Andy Lai <Andy04Lai@gmail.com>
2024-08-03 18:58:05 +0930

    text to textnodes + tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -86,4 +86,19 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"\[(.*?)\]\((.*?)\)", text)
+
+def text_to_textnodes(text):
+    node = TextNode(text, text_type_text)
+
+    new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+
+    new_nodes = split_nodes_delimiter(new_nodes, "*", text_type_italic)
+
+    new_nodes = split_nodes_delimiter(new_nodes, "`", text_type_code)
+
+    new_nodes = split_nodes_image(new_nodes)
+
+    new_nodes = split_nodes_link(new_nodes)
+
+    return new_nodes
    
