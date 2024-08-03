3042c237f130cf2fd7bfd565d6d3935481140bc3
Andy Lai <Andy04Lai@gmail.com>
2024-08-03 21:58:24 +0930

    block to type + Tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,9 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
@@ -10,6 +16,45 @@ def markdown_to_blocks(markdown):
     return filtered_blocks
 
 
-def block_to_block_type():
-    pass
-
+def block_to_type(block):
+    lines = block.split("\n")
+    # Headings
+    if (
+        block.startswith("# ")
+        or block.startswith("## ")
+        or block.startswith("### ")
+        or block.startswith("#### ")
+        or block.startswith("##### ")
+        or block.startswith("###### ")
+    ):
+        return block_type_heading
+    # Code Block
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return block_type_code
+    # Quote Block
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+    # Unordered List
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return block_type_paragraph
+        return block_type_ulist
+    # Unordered List
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return block_type_paragraph
+        return block_type_ulist
+    # Ordered List
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return block_type_paragraph
+            i += 1
+        return block_type_olist
+    return block_type_paragraph
